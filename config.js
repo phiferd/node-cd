e83c0f50488eba0b4003c7421df8508e3d6fd0b7
Dan Phifer <dan.phifer@gmail.com>
2017-05-28 13:45:14 +0800

    cleaning up

--- config.js
+++ config.js
@@ -3,29 +3,7 @@ const Private = {
     port:  process.env.NODE_CD_PORT || '61440'
   },
   security: {
-    authorizedIps: [
-      '127.0.0.1',
-      'localhost'
-    ],
-    bitbucketIps: [
-		'131.103.20.160/27',
-		'165.254.145.0/26',
-		'104.192.143.0/24'
-    ],
-    githubIps: [
-      '207.97.227.253',
-      '50.57.128.197',
-      '204.232.175.75',
-      '108.171.174.178'
-    ],
-    githubAuthorizedSubnets: [
-      '204.232.175.64/27',
-      '192.30.252.0/22'
-    ],
     key: process.env.NODE_CD_DEPLOY_KEY || ""
-  },
-  repository: {
-    branch: 'master'
   },
   action: {
     exec: {
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-exports.index = function (req, res) {
-  res.json({status: 'ok'})
-}
-
-exports.favicon = function (req, res) {
-  res.writeHead(404)
-  res.end()
-}
